7e63d0b86b383f8ebcf5a8088090ceaabbbc294b
AnkurKumar160305 <ankurstyle19@gmail.com>
2025-08-27 11:25:12 +0530

    added new files

---
+++ c-RecursionandDivideandConquer/78-Subsets.cpp
@@ -0,0 +1,24 @@
+// Time Complexity: O(n × 2^n)
+// Space Complexity: O(n × 2^n) (dominated by result storage)
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> arr;
+        int n=nums.size();
+        int p = 1 << n;  // total number of subsets = 2^n
+
+        for(int i=0;i<p;i++){
+            vector<int> subset;
+            for(int j=0;j<n;j++){
+                if(i & (1<<j)){  //checks if the j-th bit in the number i is set (1) or not (0).
+                    subset.push_back(nums[j]);
+                }
+            }
+            arr.push_back(subset);
+        }
+        return arr;
+    }
+};
+
+
+// optimal (memory-friendly) approach using backtracking (DFS)
